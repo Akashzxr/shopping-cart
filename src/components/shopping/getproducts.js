1f0f27fd5ff6d0307cdcdb675456762eda58341a
Akash ks <akashks8855@gmail.com>
2022-10-23 15:11:52 +0530

    updated code for fetching and display data

--- src/components/shopping/getproducts.js
+++
@@ -1,7 +0,0 @@
-const fetchProducts = async () => {
-    const response = await fetch('https://fakestoreapi.com/products/category/men\'s clothing')
-    let data = await response.json()
-    return data
-  }
-
-  export default fetchProducts;
--- src/components/shopping/items.js
+++ src/components/shopping/items.js
@@ -1,19 +1,16 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "./productcard";
-import fetchProducts from "./getproducts";
+
 
 const Items=()=>{
 
 const[data,setdata] = useState([]);
-const[filter,setfilter]=useState(data);
 
 const fetchProducts = async () => {
     const response = await fetch('https://fakestoreapi.com/products/category/men\'s clothing')
-        setdata(await response.clone().json());
-        setfilter(await response.json());
-        console.log(filter);
+        setdata(await response.json());
+        console.log(data);
   }
-
 
   useEffect(()=>{
     fetchProducts();
@@ -21,11 +18,10 @@ const fetchProducts = async () => {
 
     return(
         <div className="items" >
-            {filter.map((product)=>{
+            {data.map((product)=>{
                 return(
                     <ProductCard key={product.id} info={product}/>
                 )
-                
             })}
         </div>
     )
